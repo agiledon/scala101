710459069344e4fa187bbce5c12709107f6edaa0
zhangyi <bruce.e.zhang@gmail.com>
2014-08-10 16:49:03 +0800

    use spark streaming

--- src/main/scala/com/agiledon/scala/framework/spark/streaming/HdfsWordCount.scala
+++ src/main/scala/com/agiledon/scala/framework/spark/streaming/HdfsWordCount.scala
@@ -2,10 +2,16 @@ package com.agiledon.scala.framework.spark.streaming
 
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.SparkConf
 
+//how to run this app
+//use spark-submit like this:
+//./spark-submit --class com.agiledon.scala.framework.spark.streaming.HdfsWordCount --master local /Users/twer/workspace/scala101/target/scala-2.10/scala101_2.10-0.1.jar 100
 object HdfsWordCount extends App {
-  val ssc = new StreamingContext("local[2]", "HdfsWordCount", Seconds(2))
-  val lines = ssc.textFileStream("samples")
+  val sparkConf = new SparkConf().setAppName("HdfsWordCount")
+//  val ssc = new StreamingContext("local[2]", "HdfsWordCount", Seconds(5))
+  val ssc = new StreamingContext(sparkConf, Seconds(5))
+  val lines = ssc.textFileStream("/Users/twer/workspace/scala101/data")   //local directory
   val wordCount = lines.flatMap(_.split(" ")).map(x => (x, 1)).reduceByKey(_ + _)
 
   wordCount.print()
